443e49f9f06fd2e5528c789339969a3c9df9674f
Tyler LeBlond <tylerleblond@gmail.com>
2023-01-19 13:43:47 -0500

    Cleaned up repository.

--- src/layout/dynamic_layouts/determine_exposed_operators.cpp
+++ src/layout/dynamic_layouts/determine_exposed_operators.cpp
@@ -22,6 +22,7 @@ tsl::ordered_map<PatchId, RotatableSingleQubitPatchExposedOperators> determine_e
 {
     tsl::ordered_map<PatchId, RotatableSingleQubitPatchExposedOperators> out;
 
+    // TRL 01/16/22: Since we haven't implemented the edpc layout flag here yet, we just pass a dummy boolean
     bool dummy = 0;
     DenseSlice first_slice{layout, core_qubit_ids, dummy};
 
