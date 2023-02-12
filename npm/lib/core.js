1ad5eaa3186f41360363c3d78e253dc27b349401
Alex Nguyen <hoangminh98@gmail.com>
2023-02-12 14:43:12 -0800

    Merging both functions as one

--- npm/lib/core.js
+++ npm/lib/core.js
@@ -1,42 +1,29 @@
 const LsqeccModule = require('../wasm/lsqecc_emscripten.js');
 
 /**
- * Runs the slicer from QASM. Only supports a small subset of OpenQASM 2.0. See the README for details.
+ * Runs the slicer from string input.
  * 
- * @param {string} input QASM circuit content as string
+ * @param {string} input Circuit content as string
+ * @param {string} inputType 'qasm' for QASM input, 'lli' for LLI input. 
  * @param {boolean} compactLayout Use Litinski's compact layout. Default is 'false'.
  * @param {string} cnotCorrections Add Xs and Zs to correct the the negative outcomes: 'never' (default), 'always'
  * @returns {{err: string, exit_code: number, output: string}} Slicer result
  * 
  */
-const slicerFromQasm = async (input, compactLayout=false, cnotCorrections='never') => {
+async function runSlicer(input, inputType='lli', compactLayout=false, cnotCorrections='never') {
     const loadedModule = await LsqeccModule();
+
+    if (inputType !== 'qasm' && inputType !== 'lli') {
+        throw new Error(`Invalid argument inputType: ${inputType}`);
+    }
+
+    if (cnotCorrections !== 'never' && cnotCorrections !== 'always') {
+        throw new Error(`Invalid argument cnotCorrections: ${cnotCorrections}`);
+    }
 
     const commandLineArgs = [
         compactLayout ? '--compactlayout' : '',
-        '-q',
-        '--cnotcorrections',
-        cnotCorrections === 'always' ? 'always' : 'never',
-    ];
-
-    const result = loadedModule.run_slicer_program_from_strings(commandLineArgs.join(' '), input);
-
-    return result;
-}
-
-/**
- * Runs the slicer from LLI instruction.
- * 
- * @param {string} input QASM circuit content as string
- * @param {boolean} compactLayout Use Litinski's compact layout. Default is 'false'.
- * @param {string} cnotCorrections Add Xs and Zs to correct the the negative outcomes: 'never' (default), 'always'
- * @returns {{err: string, exit_code: number, output: string}} Slicer result
- */
-const slicerFromLLI = async (input, compactLayout=false, cnotCorrections='never') => {
-    const loadedModule = await LsqeccModule();
-
-    const commandLineArgs = [
-        compactLayout ? '--compactlayout' : '',
+        inputType === 'qasm' ? '-q' : '',
         '--cnotcorrections',
         cnotCorrections === 'always' ? 'always' : 'never',
     ];
@@ -47,6 +34,5 @@ const slicerFromLLI = async (input, compactLayout=false, cnotCorrections='never'
 }
 
 module.exports = {
-    slicerFromQasm,
-    slicerFromLLI,
+    runSlicer,
 };
