7f0c3cf9de587462a32a80472290014bb8741967
Alex Nguyen <hoangminh98@gmail.com>
2023-02-20 22:01:43 -0800

    Update to use Slicer class

--- npm/src/index.ts
+++ npm/src/index.ts
@@ -1,38 +1,68 @@
-const LsqeccModule = require('../wasm/lsqecc_emscripten.js');
+// @ts-ignore
+import LsqeccModule from "../wasm/lsqecc_emscripten.js";
 
-/**
- * Runs the slicer from string input.
- * 
- * @param {string} input Circuit content as string
- * @param {string} inputType 'qasm' for QASM input, 'lli' (default) for LLI input.
- * @param {string} layoutGenerator Choose the type of layout generator: 'compact' (default), 'edpc'
- * @param {string} cnotCorrections Add Xs and Zs to correct the the negative outcomes: 'never' (default), 'always'
- * @returns {{err: string, exit_code: number, output: string}} Slicer result
- * 
- */
-async function runSlicer(input, inputType='lli', layoutGenerator='', cnotCorrections='never') {
-    const loadedModule = await LsqeccModule();
+type inputType = "lli" | "qasm";
 
-    if (inputType !== 'qasm' && inputType !== 'lli') {
-        throw new Error(`Invalid argument inputType: ${inputType}`);
-    }
+type cnotCorrections = "never" | "always";
 
-    if (cnotCorrections !== 'never' && cnotCorrections !== 'always') {
-        throw new Error(`Invalid argument cnotCorrections: ${cnotCorrections}`);
-    }
+type layoutGenerator = "compact" | "edpc" | "";
 
-    const commandLineArgs = [
-        layoutGenerator ? `-L ${layoutGenerator}` : "",
-        inputType === 'qasm' ? '-q' : '',
-        '--cnotcorrections',
-        cnotCorrections === 'always' ? 'always' : 'never',
-    ];
-
-    const result = loadedModule.run_slicer_program_from_strings(commandLineArgs.join(' '), input);
-
-    return result;
+interface slicerResult {
+    err: string;
+    exit_code: number;
+    output: string;
 }
 
-module.exports = {
-    runSlicer,
-};
+class Slicer {
+    private constructor(protected _module: any) {}
+
+    /**
+     * Loads and initializes the WASM module.
+     *
+     * @returns Promise that resolves to a Slicer instance
+     */
+    static load(): Promise<Slicer> {
+        return LsqeccModule().then((module: any) => new Slicer(module));
+    }
+
+    /**
+     * Runs the slicer from string input.
+     *
+     * @param input Circuit content as string
+     * @param inputType 'qasm' for QASM input, 'lli' (default) for LLI input.
+     * @param layoutGenerator Choose the type of layout generator: 'compact' (default), 'edpc'
+     * @param cnotCorrections Add Xs and Zs to correct the the negative outcomes: 'never' (default), 'always'
+     * @returns Slicer result
+     *
+     */
+    run(
+        input: string,
+        inputType: inputType = "lli",
+        layoutGenerator: layoutGenerator = "",
+        cnotCorrections: cnotCorrections = "never"
+    ): slicerResult {
+        if (inputType !== "qasm" && inputType !== "lli") {
+            throw new Error(`Invalid argument inputType: ${inputType}`);
+        }
+
+        if (cnotCorrections !== "never" && cnotCorrections !== "always") {
+            throw new Error(`Invalid argument cnotCorrections: ${cnotCorrections}`);
+        }
+
+        const commandLineArgs = [
+            layoutGenerator ? `-L ${layoutGenerator}` : "",
+            inputType === "qasm" ? "-q" : "",
+            "--cnotcorrections",
+            cnotCorrections === "always" ? "always" : "never",
+        ];
+
+        const result = this._module.run_slicer_program_from_strings(
+            commandLineArgs.join(" "),
+            input
+        );
+
+        return result;
+    }
+}
+
+export default Slicer;
