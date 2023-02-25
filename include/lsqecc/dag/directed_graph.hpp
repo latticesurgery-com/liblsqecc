df258ebe19c65e996c558d7c19453fa303409617
George Watkins <gwwatkin@sfu.ca>
2023-02-25 09:56:10 -0800

    Add basic DirectedGraph structure

---
+++ include/lsqecc/dag/directed_graph.hpp
@@ -0,0 +1,58 @@
+#include <algorithm>
+#include <tsl/ordered_map.h>
+#include <tsl/ordered_set.h>
+#include <vector>
+#include <ostream>
+
+namespace lsqecc {
+
+namespace dag {
+
+using label_t = size_t;
+
+template <typename K, typename V>
+using Map = tsl::ordered_map<K, V>;
+
+template <typename T>
+using Set = tsl::ordered_set<T>;
+
+
+struct DirectedGraph 
+{
+    void add_node(label_t label);
+
+    void add_edge(label_t from, label_t to);
+
+    void remove_edge(label_t from, label_t to);
+
+    void remove_node(label_t target);
+
+    void subdivide(label_t target, const std::vector<label_t>& replacement);
+
+    Set<label_t> heads() const;
+
+    Set<label_t> tails() const;
+
+    std::ostream& to_graphviz(std::ostream& os) const;
+
+private:
+    // Minimal definition
+    Map<label_t, Set<label_t>> edges_;
+    
+    // Cached on the fly
+    Map<label_t, Set<label_t>> back_edges_;
+
+
+    friend const Map<label_t, Set<label_t>>& get_edges_for_testing(const DirectedGraph& g);
+    friend const Map<label_t, Set<label_t>>& get_back_edges_for_testing(const DirectedGraph& g);
+};
+
+
+const Map<label_t, Set<label_t>>& get_edges_for_testing(const DirectedGraph& g);
+const Map<label_t, Set<label_t>>& get_back_edges_for_testing(const DirectedGraph& g);
+
+} // namespace dag
+
+} // namespace lsqecc
+
+
