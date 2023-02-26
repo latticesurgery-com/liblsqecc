56901e44bc50d1b7d5ed478a6b531efaf6669cb3
George Watkins <36427091+gwwatkin@users.noreply.github.com>
2023-02-26 14:39:19 -0800

    Add basic DirectedGraph structure (#63)

---
+++ src/dag/directed_graph.cpp
@@ -0,0 +1,166 @@
+#include <lsqecc/dag/directed_graph.hpp>
+
+#include <stdexcept>
+
+
+namespace lsqecc::dag {
+
+void DirectedGraph::add_node(label_t label)
+{
+    if(!edges_.count(label))
+    {
+        edges_.insert_or_assign(label, Set<label_t>());
+        back_edges_.insert_or_assign(label, Set<label_t>());
+    }
+}
+
+void DirectedGraph::add_edge(label_t from, label_t to)
+{
+    add_node(to);
+    add_node(from);
+    edges_.at(from).insert(to);
+    back_edges_.at(to).insert(from);
+}
+
+void DirectedGraph::remove_edge(label_t from, label_t to)
+{
+    if(!edges_.count(from))
+        throw std::runtime_error("Cannot remove edge from non-existing node: " + std::to_string(from));
+
+    if(!edges_.count(to))
+        throw std::runtime_error("Cannot remove edge to non-existing node: " + std::to_string(to));
+
+    edges_.at(from).erase(std::find(edges_.at(from).begin(), edges_.at(from).end(), to));
+    back_edges_.at(to).erase(std::find(back_edges_.at(to).begin(), back_edges_.at(to).end(), from));
+}
+
+void DirectedGraph::remove_node(label_t target)
+{
+    if(!edges_.count(target))
+        throw std::runtime_error("Cannot remove non-existing node: " + std::to_string(target));
+
+    for(const auto& to : edges_.at(target))
+        remove_edge(target, to);
+
+    for(const auto& from : back_edges_.at(target))
+        remove_edge(from, target);
+
+    edges_.erase(target);
+    back_edges_.erase(target);
+}
+
+void DirectedGraph::subdivide(label_t target, const std::vector<label_t>& replacement)
+{
+    if(!edges_.count(target))
+        throw std::runtime_error("Cannot subdivide non-existing node: " + std::to_string(target));
+
+    if(replacement.size() < 2)
+        throw std::runtime_error("Cannot subdivide into less than 2 nodes");
+
+    for(const auto& label : replacement)
+    {
+        if(edges_.count(label))
+            throw std::runtime_error("Cannot subdivide into existing node");
+    }
+
+    for(std::size_t i = 1; i != replacement.size(); ++i)
+        add_edge(replacement.at(i-1), replacement.at((i)));
+
+    Set<label_t> forward_dangling;
+    for(const auto& to : edges_.at(target))
+        forward_dangling.insert(to);
+    for(const auto& to : forward_dangling)
+        remove_edge(target, to);
+    for(const auto& to : forward_dangling)
+        add_edge(replacement.back(), to);
+
+    Set<label_t> backward_dangling;
+    for(const auto& from : back_edges_.at(target))
+        backward_dangling.insert(from);
+    for(const auto& from : backward_dangling)
+        remove_edge(from, target);
+    for(const auto& from : backward_dangling)
+        add_edge(from, replacement.front());
+
+    remove_node(target);
+}
+
+Set<label_t> DirectedGraph::heads() const
+{
+    Set<label_t> heads;
+    for(const auto& [label, neighbors] : edges_)
+    {
+        if(back_edges_.at(label).size() == 0)
+            heads.insert(label);
+    }
+    return heads;
+}
+
+Set<label_t> DirectedGraph::tails() const
+{
+    Set<label_t> tails;
+    for(const auto& [label, neighbors] : edges_)
+    {
+        if(edges_.at(label).size() == 0)
+            tails.insert(label);
+    }
+    return tails;
+}
+
+
+void DirectedGraph::topological_order_helper(label_t current, Set<label_t>& visited, std::vector<label_t>& order) const
+{
+    if(visited.count(current))return;
+
+    visited.insert(current);
+
+    for(const auto& neighbor : edges_.at(current))
+        topological_order_helper(neighbor, visited, order);
+
+    order.push_back(current);
+}
+
+
+
+std::vector<label_t> DirectedGraph::topological_order_tails_first() const
+{
+    std::vector<label_t> order;
+    Set<label_t> visited;
+    for(const auto& head : heads())
+        topological_order_helper(head, visited, order);
+    return order;
+}
+
+
+
+std::ostream& DirectedGraph::to_graphviz(std::ostream& os) const
+{
+
+    os << "digraph DirectedGraph {" << std::endl;
+
+    // Print all nodes
+    for(const auto& [label, neighbors] : edges_)
+        os << "  " << label << ";" << std::endl;
+
+    // Print all edges
+    for(const auto& [from, neighbors] : edges_)
+        for(const auto& to : neighbors)
+            os << "  " << from << " -> " << to << ";" << std::endl;
+
+    os << "}" << std::endl;
+    return os;
+}
+
+
+const Map<label_t, Set<label_t>>& get_edges_for_testing(const DirectedGraph& g)
+{
+    return g.edges_;
+}
+
+const Map<label_t, Set<label_t>>& get_back_edges_for_testing(const DirectedGraph& g)
+{
+    return g.back_edges_;
+}
+
+
+} // namespace lsqecc
---
+++ tests/dag/directed_graph.cpp
@@ -0,0 +1,237 @@
+#include <gtest/gtest.h>
+
+#include <lsqecc/dag/directed_graph.hpp>
+
+using namespace lsqecc::dag;
+
+TEST(directed_graph, add_node)
+{
+    DirectedGraph g;
+    g.add_node(0);
+    g.add_node(1);
+    g.add_node(2);
+    g.add_node(2);
+    ASSERT_EQ(3, get_back_edges_for_testing(g).size());    
+    ASSERT_EQ(3, get_edges_for_testing(g).size());    
+    ASSERT_EQ(0, get_back_edges_for_testing(g).at(0).size());    
+    ASSERT_EQ(0, get_edges_for_testing(g).at(0).size());    
+    ASSERT_EQ(0, get_back_edges_for_testing(g).at(1).size());    
+    ASSERT_EQ(0, get_edges_for_testing(g).at(1).size());    
+    ASSERT_EQ(0, get_back_edges_for_testing(g).at(2).size());    
+    ASSERT_EQ(0, get_edges_for_testing(g).at(2).size());    
+
+}
+
+TEST(directed_graph, add_edge)
+{
+    DirectedGraph g;
+    g.add_edge(0, 1);
+    ASSERT_EQ(2, get_back_edges_for_testing(g).size());
+    ASSERT_EQ(2, get_edges_for_testing(g).size());
+    ASSERT_EQ(Set<label_t>{1}, get_edges_for_testing(g).at(0));
+    ASSERT_EQ(Set<label_t>{}, get_edges_for_testing(g).at(1));
+    ASSERT_EQ(Set<label_t>{0}, get_back_edges_for_testing(g).at(1));
+    ASSERT_EQ(Set<label_t>{}, get_back_edges_for_testing(g).at(0));
+
+    g.add_edge(1, 2);
+    ASSERT_EQ(3, get_back_edges_for_testing(g).size());
+    ASSERT_EQ(3, get_edges_for_testing(g).size());
+    ASSERT_EQ(Set<label_t>{1}, get_edges_for_testing(g).at(0));
+    ASSERT_EQ(Set<label_t>{2}, get_edges_for_testing(g).at(1));
+    ASSERT_EQ(Set<label_t>{}, get_edges_for_testing(g).at(2));
+    ASSERT_EQ(Set<label_t>{0}, get_back_edges_for_testing(g).at(1));
+    ASSERT_EQ(Set<label_t>{1}, get_back_edges_for_testing(g).at(2));
+    ASSERT_EQ(Set<label_t>{}, get_back_edges_for_testing(g).at(0));
+
+
+    g.add_edge(0, 2);
+    ASSERT_EQ(3, get_back_edges_for_testing(g).size());
+    ASSERT_EQ(3, get_edges_for_testing(g).size());
+    Set<label_t> v{1, 2};
+    ASSERT_EQ(v, get_edges_for_testing(g).at(0));
+    ASSERT_EQ(Set<label_t>{2}, get_edges_for_testing(g).at(1));
+    ASSERT_EQ(Set<label_t>{}, get_edges_for_testing(g).at(2));
+    ASSERT_EQ(Set<label_t>{0}, get_back_edges_for_testing(g).at(1));
+    Set<label_t> v2{1, 0};
+    ASSERT_EQ(v2, get_back_edges_for_testing(g).at(2));
+    ASSERT_EQ(Set<label_t>{}, get_back_edges_for_testing(g).at(0));
+}
+
+
+TEST(directed_graph, heads_and_tails)
+{
+    DirectedGraph g;
+    g.add_edge(0, 2);
+    g.add_edge(1, 2);
+    g.add_edge(2, 3);
+    g.add_edge(2, 4);
+    /* Graph (all edges pointing downwards):
+    0   1
+     \ /
+      2
+     / \
+    3   4
+    */
+    Set<label_t> heads{0,1};
+    ASSERT_EQ(heads, g.heads());
+    Set<label_t> tails{3,4};
+    ASSERT_EQ(tails, g.tails());
+}
+
+TEST(directed_graph, heads_and_tails_after_update)
+{
+    DirectedGraph g;
+    g.add_edge(0, 2);
+    g.add_edge(1, 2);
+    g.add_edge(2, 3);
+    g.add_edge(2, 4);
+    /* Graph (all edges pointing downwards):
+    0   1
+     \ /
+      2
+     / \
+    3   4
+    */
+
+    g.add_edge(100, 0);
+    g.remove_node(3);
+    /* Graph after update (all edges pointing downwards):
+    100
+    |
+    0   1
+     \ /
+      2
+       \
+        4
+    */
+
+    Set<label_t> heads{1, 100};
+    ASSERT_EQ(heads, g.heads());
+    Set<label_t> tails{4};
+    ASSERT_EQ(tails, g.tails());
+}
+
+
+TEST(directed_graph, subdivide)
+{
+    DirectedGraph g;
+    g.add_edge(0, 2);
+    g.add_edge(1, 2);
+    g.add_edge(2, 3);
+    g.add_edge(2, 4);
+    /* Graph (all edges pointing downwards):
+    0   1
+     \ /
+      2
+     / \
+    3   4
+    */
+
+    g.subdivide(2, {100, 101, 102});
+
+    /* Graph after subdivide (all edges pointing downwards):
+    0   1
+     \ /
+     100
+      |
+     101 
+      |
+     102
+     / \
+    3   4
+    */
+
+    ASSERT_EQ(7, get_back_edges_for_testing(g).size());
+    ASSERT_EQ(7, get_edges_for_testing(g).size());
+    ASSERT_EQ(Set<label_t>{100}, get_edges_for_testing(g).at(0));
+    ASSERT_EQ(Set<label_t>{100}, get_edges_for_testing(g).at(1));
+    ASSERT_EQ(Set<label_t>{101}, get_edges_for_testing(g).at(100));
+    ASSERT_EQ(Set<label_t>{102}, get_edges_for_testing(g).at(101));
+    Set<label_t> v{3, 4};
+    ASSERT_EQ(v, get_edges_for_testing(g).at(102));
+    ASSERT_EQ(Set<label_t>{}, get_edges_for_testing(g).at(3));
+    ASSERT_EQ(Set<label_t>{}, get_edges_for_testing(g).at(4));
+
+    ASSERT_EQ(Set<label_t>{}, get_back_edges_for_testing(g).at(0));
+    ASSERT_EQ(Set<label_t>{}, get_back_edges_for_testing(g).at(1));
+    Set<label_t> v2{0, 1};
+    ASSERT_EQ(v2, get_back_edges_for_testing(g).at(100));
+    ASSERT_EQ(Set<label_t>{100}, get_back_edges_for_testing(g).at(101));
+    ASSERT_EQ(Set<label_t>{101}, get_back_edges_for_testing(g).at(102));
+    ASSERT_EQ(Set<label_t>{102}, get_back_edges_for_testing(g).at(3));
+    ASSERT_EQ(Set<label_t>{102}, get_back_edges_for_testing(g).at(4));
+}
+
+TEST(directed_graph, topological_order_tails_first_1)
+{
+    DirectedGraph g;
+    g.add_edge(0, 2);
+    g.add_edge(1, 2);
+    g.add_edge(2, 3);
+    g.add_edge(2, 4);
+    /* Graph (all edges pointing downwards):
+    0   1
+     \ /
+      2
+     / \
+    3   4
+    */
+    std::vector<label_t> topological_order{3, 4, 2, 0, 1};
+    ASSERT_EQ(topological_order, g.topological_order_tails_first());
+}
+
+TEST(directed_graph, topological_order_tails_first_2)
+{
+    DirectedGraph g;
+    g.add_edge(0, 2);
+    g.add_edge(1, 2);
+    g.add_edge(2, 3);
+    g.add_edge(2, 4);
+    g.add_edge(3, 5);
+    g.add_edge(4, 5);
+    g.add_edge(5, 9);
+    g.add_edge(6, 7);
+    g.add_edge(7, 4);
+    g.add_edge(7, 8);
+    g.add_edge(8, 9);
+    g.add_edge(10, 11);
+
+    /* Graph (all edges pointing downwards):
+    0   1   6   10
+     \ /   /     |
+      2   7     11
+     / \ / \ 
+    3   4   8
+     \ /   /
+      5   /
+       \ /
+        9
+
+    The opological order chosen by the algorithm:
+        10
+        |
+        11
+     
+        6
+        |   
+       .7  
+      / |  
+     /  8. 
+    |     \  
+    |  .1  \ 
+    | /     |          
+    | | 0   |
+    | \ |   |
+    |  >2   |
+    | / |   | 
+    | | 3   | 
+    \ \  \  |
+     `-`4 | | 
+        |/  |
+        5  /
+        | /
+        9
+    */
+    std::vector<label_t> topological_order{ 9, 5, 3, 4, 2, 0, 1, 8, 7, 6, 11, 10 };
+    ASSERT_EQ(topological_order, g.topological_order_tails_first());
+}
