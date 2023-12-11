1374b4b21e07e18764d6b01f8b9e884025156617
George Watkins <gwwatkin@sfu.ca>
2023-12-11 13:25:14 -0800

    Update crossings example

--- examples/3d_crossings/run.sh
+++ examples/3d_crossings/run.sh
@@ -1,6 +1,7 @@
 i=0
-while [ $i -le 7 ]
+while [ $i -le 8 ]
 do
-    ../../build/lsqecc_slicer -l crossings.layout -i crossings_$(($i)).lli -P 3d -o crossings_$(($i)).json
+    ../../build/lsqecc_slicer -l crossings.layout -i crossings_$(($i)).lli -P 3d -o crossings_3d_$(($i)).json
+    ../../build/lsqecc_slicer -l crossings.layout -i crossings_$(($i)).lli -P dag -o crossings_dag_$(($i)).json
     i=$(($i+1))
 done
