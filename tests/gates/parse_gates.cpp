a70889a1b1dfc6edb42ad3d55299971e922bff3a
alexandru paler <alexandrupaler@gmail.com>
2024-02-24 20:27:12 +0200

    repair test?

--- tests/gates/parse_gates.cpp
+++ tests/gates/parse_gates.cpp
@@ -12,11 +12,13 @@ TEST(parse_gates, rz_pi_over_1)
     ParseGateResult res = parse_gate(gate);
     ASSERT_TRUE(std::holds_alternative<gates::Gate>(res));
     auto g = std::get<gates::Gate>(res);
-    ASSERT_TRUE(std::holds_alternative<gates::RZ>(g));
-    auto rz = std::get<gates::RZ>(g);
+//    ASSERT_TRUE(std::holds_alternative<gates::RZ>(g));
+    ASSERT_TRUE(std::holds_alternative<gates::BasicSingleQubitGate>(g));
+    auto rz = std::get<gates::BasicSingleQubitGate>(g);
     ASSERT_EQ(22, rz.target_qubit);
-    ASSERT_EQ(1, rz.pi_fraction.num);
-    ASSERT_EQ(1, rz.pi_fraction.den);
+    ASSERT_EQ(gates::BasicSingleQubitGate::Type::Z, rz.gate_type);
+//    ASSERT_EQ(1, rz.pi_fraction.num);
+//    ASSERT_EQ(1, rz.pi_fraction.den);
 }
 
 TEST(parse_gates, rz_2_pi_over_7)
